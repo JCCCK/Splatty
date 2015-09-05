c471b6784f4225976c8c0fa98befb51265976dc0
casthewiz <cschaufe@kent.edu>
2015-09-05 13:11:08 -0400

    Repointed to Heroku (again)

--- public/ts/main.ts
+++ public/ts/main.ts
@@ -1,5 +1,5 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv', { preload: preload, create: create, update: update, render: render });
-var socket = io.connect('http://localhost:5000');
+var socket = io.connect('https://secret-temple-3770.herokuapp.com/');
 //DELETE FROM HERE AFTER VERIFIED
 var UiPlayers = document.getElementById("players");
 socket.on('count', function (data) {
