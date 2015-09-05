d590116068892645e27fe34992cc74b488ae0fc5
casthewiz <cschaufe@kent.edu>
2015-09-05 00:07:15 -0400

    Goodbye sockets, we got a garage server

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var express = require('express');
-var app = express();
+var app = require('express'),
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,5 +15,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
