2dbe9f67d467fefe20012bb91ae7e1caa2fa6311
casthewiz <cschaufe@kent.edu>
2015-09-05 00:32:51 -0400

    Nvm I'm an idiot, express fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-var app = require('express');
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
