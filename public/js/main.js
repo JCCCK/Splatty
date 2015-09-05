7f55629dabc3e856aa9c9ccc64edcd6541b86b86
casthewiz <cschaufe@kent.edu>
2015-09-05 02:27:17 -0400

    Fixed order of js, added garageserver clientside to main.js, added footer.ejs

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,15 @@ var game = new Phaser.Game(720, 480, Phaser.AUTO, 'gameDiv');
 game.state.add('main', mainState);
 game.state.start('main');
 game.physics.startSystem(Phaser.Physics.ARCADE);
+
+
+
+GarageServerIO.initializeGarageServer('insertmygameserverurlhere.com', { /* options */ });
+
+// Inside game loop...
+GarageServerIO.addInput(/*player input - in this example 'left' or 'right'*/);
+
+var playerStates = GarageServerIO.getPlayerStates();
+playerStates.forEach(function (player) {
+    ctxCanvas.fillRect(player.state.x, 0, 5, 5);
+});
