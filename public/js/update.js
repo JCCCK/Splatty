1a7afe8f7215851d5ff67efa1900413d2676a365
Kevin Novak <kevinmnovak@yahoo.com>
2015-09-06 01:38:32 -0400

    XBOX updates for update.ts/js

--- public/js/update.js
+++ public/js/update.js
@@ -6,10 +6,10 @@ function update() {
     if (!(players[sessionID] === undefined)) {
         players[sessionID].body.velocity.x = 0;
         game.physics.arcade.collide(players[sessionID], mainTileLayer);
-        if (cursors.left.isDown || aKey.isDown) {
+        if (cursors.left.isDown || aKey.isDown || (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)) {
             players[sessionID].body.velocity.x = -150;
         }
-        else if (cursors.right.isDown || dKey.isDown) {
+        else if (cursors.right.isDown || dKey.isDown || (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)) {
             players[sessionID].body.velocity.x = 150;
         }
         else {
@@ -24,9 +24,37 @@ function update() {
                 facing = 'idle';
             }
         }
-        if ((spacebar.isDown || cursors.up.isDown || wKey.isDown) && players[sessionID].body.onFloor() && game.time.now > jumpTimer) {
+        if ((spacebar.isDown || cursors.up.isDown || wKey.isDown || (pad1.justPressed(Phaser.Gamepad.XBOX360_A))) && players[sessionID].body.onFloor() && game.time.now > jumpTimer) {
             players[sessionID].body.velocity.y = -300;
             jumpTimer = game.time.now + 750;
+        }
+        if (pad1.connected) {
+            var rightStickX1 = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY1 = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+            if (rightStickX1 || rightStickY1) {
+                fire();
+            }
+        }
+        if (pad2.connected) {
+            var rightStickX2 = pad2.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY2 = pad2.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+            if (rightStickX2 || rightStickY2) {
+                fire();
+            }
+        }
+        if (pad3.connected) {
+            var rightStickX3 = pad3.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY3 = pad3.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+            if (rightStickX3 || rightStickY3) {
+                fire();
+            }
+        }
+        if (pad4.connected) {
+            var rightStickX4 = pad4.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY4 = pad4.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+            if (rightStickX4 || rightStickY4) {
+                fire();
+            }
         }
         if (game.input.activePointer.isDown) {
             fire();
@@ -57,6 +85,29 @@ function update() {
             var bullet = bullets.getFirstDead();
             var bulletTarget = {};
             bullet.reset(players[sessionID].x + 10, players[sessionID].y + 20);
+            if (pad1.connected) {
+                var angleToShoot = Math.atan2(rightStickY1, rightStickX1);
+                bullet.body.velocity.x = (Math.cos(angleToShoot) * 700);
+                bullet.body.velocity.y = (Math.sin(angleToShoot) * 700);
+            }
+            if (pad2.connected) {
+                var angleToShoot = Math.atan2(rightStickY2, rightStickX2);
+                bullet.body.velocity.x = (Math.cos(angleToShoot) * 700);
+                bullet.body.velocity.y = (Math.sin(angleToShoot) * 700);
+            }
+            if (pad3.connected) {
+                var angleToShoot = Math.atan2(rightStickY3, rightStickX3);
+                bullet.body.velocity.x = (Math.cos(angleToShoot) * 700);
+                bullet.body.velocity.y = (Math.sin(angleToShoot) * 700);
+            }
+            if (pad4.connected) {
+                var angleToShoot = Math.atan2(rightStickY4, rightStickX4);
+                bullet.body.velocity.x = (Math.cos(angleToShoot) * 700);
+                bullet.body.velocity.y = (Math.sin(angleToShoot) * 700);
+            }
+            else {
+                game.physics.arcade.moveToPointer(bullet, 700);
+            }
             game.physics.arcade.moveToPointer(bullet, 700);
             bulletTarget = {
                 x: game.input.mousePointer.x,
--- public/ts/update.ts
+++ public/ts/update.ts
@@ -28,12 +28,33 @@ function update() {
             players[sessionID].body.velocity.y = -300;
             jumpTimer = game.time.now + 750;
         }
-        var rightStickX = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
-        var rightStickY = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
         if (pad1.connected) {
-            if (rightStickX || rightStickY) {
+            var rightStickX1 = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY1 = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+              if (rightStickX1 || rightStickY1) {
                 fire();
-            }
+              }
+        }
+        if (pad2.connected) {
+            var rightStickX2 = pad2.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY2 = pad2.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+              if (rightStickX2 || rightStickY2) {
+                fire();
+              }
+        }
+        if (pad3.connected) {
+            var rightStickX3 = pad3.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY3 = pad3.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+              if (rightStickX3 || rightStickY3) {
+                fire();
+              }
+        }
+        if (pad4.connected) {
+            var rightStickX4 = pad4.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+            var rightStickY4 = pad4.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+              if (rightStickX4 || rightStickY4) {
+                fire();
+              }
         }
         if (game.input.activePointer.isDown) {
             fire();
@@ -65,7 +86,22 @@ function update() {
             var bulletTarget = {};
             bullet.reset(players[sessionID].x + 10, players[sessionID].y + 20);
             if (pad1.connected) {
-                var angleToShoot = Math.atan2(rightStickY, rightStickX);
+                var angleToShoot = Math.atan2(rightStickY1, rightStickX1);
+                bullet.body.velocity.x = (Math.cos(angleToShoot) * 700);
+                bullet.body.velocity.y = (Math.sin(angleToShoot) * 700);
+            }
+            if (pad2.connected) {
+                var angleToShoot = Math.atan2(rightStickY2, rightStickX2);
+                bullet.body.velocity.x = (Math.cos(angleToShoot) * 700);
+                bullet.body.velocity.y = (Math.sin(angleToShoot) * 700);
+            }
+            if (pad3.connected) {
+                var angleToShoot = Math.atan2(rightStickY3, rightStickX3);
+                bullet.body.velocity.x = (Math.cos(angleToShoot) * 700);
+                bullet.body.velocity.y = (Math.sin(angleToShoot) * 700);
+            }
+            if (pad4.connected) {
+                var angleToShoot = Math.atan2(rightStickY4, rightStickX4);
                 bullet.body.velocity.x = (Math.cos(angleToShoot) * 700);
                 bullet.body.velocity.y = (Math.sin(angleToShoot) * 700);
             }
