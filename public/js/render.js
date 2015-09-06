07133f851deb8672a80f92d378ba0799c0b12c86
Kevin Novak <kevinmnovak@yahoo.com>
2015-09-06 17:38:36 -0400

    Added correct facing when shooting with XBOX

--- public/js/render.js
+++ public/js/render.js
@@ -1,17 +1,35 @@
 function render() {
-    if (players[sessionID] === undefined) {
-    }
-    else {
-        if (game.input.x < players[sessionID].x - game.camera.x) {
+    if (game.input.gamepad.pad1.connected) {
+        var rightStickX1 = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+        var rightStickY1 = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+        if (rightStickX1 > 0.1) {
+            if (facing != 'right') {
+                players[sessionID].animations.play('right');
+                facing = 'right';
+            }
+        }
+        else if (rightStickX1 < -0.1) {
             if (facing != 'left') {
                 players[sessionID].animations.play('left');
                 facing = 'left';
             }
         }
+    }
+    else {
+        if (players[sessionID] === undefined) {
+        }
         else {
-            if (facing != 'right') {
-                players[sessionID].animations.play('right');
-                facing = 'right';
+            if (game.input.x < players[sessionID].x - game.camera.x) {
+                if (facing != 'left') {
+                    players[sessionID].animations.play('left');
+                    facing = 'left';
+                }
+            }
+            else {
+                if (facing != 'right') {
+                    players[sessionID].animations.play('right');
+                    facing = 'right';
+                }
             }
         }
     }
--- public/ts/render.ts
+++ public/ts/render.ts
@@ -1,18 +1,36 @@
-function render (){
-    if  (players[sessionID] === undefined){
-
-    }
-    else {
-        if (game.input.x < players[sessionID].x - game.camera.x) {
+function render () {
+    if (game.input.gamepad.pad1.connected) {
+        var rightStickX1 = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+        var rightStickY1 = pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+        if (rightStickX1 > 0.1) {
+            if (facing != 'right') {
+                players[sessionID].animations.play('right');
+                facing = 'right';
+            }
+        }
+        else if (rightStickX1 < -0.1) {
             if (facing != 'left') {
                 players[sessionID].animations.play('left');
                 facing = 'left';
             }
         }
+    }
+    else {
+        if  (players[sessionID] === undefined){
+
+        }
         else {
-            if (facing != 'right') {
-                players[sessionID].animations.play('right');
-                facing = 'right';
+            if (game.input.x < players[sessionID].x - game.camera.x) {
+                if (facing != 'left') {
+                    players[sessionID].animations.play('left');
+                    facing = 'left';
+                }
+            }
+            else {
+                if (facing != 'right') {
+                    players[sessionID].animations.play('right');
+                    facing = 'right';
+                }
             }
         }
     }
