ffaecdaf1ce2b930ced709c05c2b9afe11e62dcd
Kevin Novak <kevinmnovak@yahoo.com>
2015-09-06 01:11:18 -0400

    XBOX jump and movement.

--- public/js/update.js
+++ public/js/update.js
@@ -6,10 +6,10 @@ function update() {
     if (!(players[sessionID] === undefined)) {
         players[sessionID].body.velocity.x = 0;
         game.physics.arcade.collide(players[sessionID], mainTileLayer);
-        if (cursors.left.isDown || aKey.isDown) {
+        if (cursors.left.isDown || aKey.isDown || (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)) {
             players[sessionID].body.velocity.x = -150;
         }
-        else if (cursors.right.isDown || dKey.isDown) {
+        else if (cursors.right.isDown || dKey.isDown || (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)) {
             players[sessionID].body.velocity.x = 150;
         }
         else {
@@ -24,7 +24,7 @@ function update() {
                 facing = 'idle';
             }
         }
-        if ((spacebar.isDown || cursors.up.isDown || wKey.isDown) && players[sessionID].body.onFloor() && game.time.now > jumpTimer) {
+        if ((spacebar.isDown || cursors.up.isDown || wKey.isDown || (pad1.justPressed(Phaser.Gamepad.XBOX360_A))) && player.body.onFloor() && game.time.now > jumpTimer) {
             players[sessionID].body.velocity.y = -300;
             jumpTimer = game.time.now + 750;
         }
